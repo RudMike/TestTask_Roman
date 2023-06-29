313765b861c925ed319a69899da6eb5fdf085c92
RudMike <nesdavaisa@gmail.com>
2023-06-29 21:56:52 +0300

    Register services in DI

--- TestTask_Roman/Program.cs
+++ TestTask_Roman/Program.cs
@@ -7,6 +7,16 @@
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using TestTask_Roman.Data.Contexts;
+using TestTask_Roman.Data.Models;
+using TestTask_Roman.Data.Repositories;
+using TestTask_Roman.Domain;
+using TestTask_Roman.Infrastructure;
+using TestTask_Roman.Infrastructure.Middleware;
+using TestTask_Roman.Infrastructure.Repositories;
+using TestTask_Roman.Infrastructure.Services;
+using TestTask_Roman.Infrastructure.Validators;
+
 namespace TestTask_Roman
 {
     /// <summary>
@@ -45,6 +55,19 @@ namespace TestTask_Roman
                     },
                 });
             });
+            _ = builder.Services.AddScoped<Func<MedicalDbContext>>(provider => () => provider.GetService<MedicalDbContext>()!);
+            _ = builder.Services.AddScoped<DbContextFactory>();
+            _ = builder.Services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+            _ = builder.Services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            _ = builder.Services.AddScoped(typeof(IDatabaseService<>), typeof(DatabaseBaseService<>));
+            _ = builder.Services.AddScoped<IEntityValidator<Patient>, PatientValidator>();
+            _ = builder.Services.AddScoped<IEntityValidator<Doctor>, DoctorValidator>();
+            _ = builder.Services.AddScoped<IPatientRepository, PatientsRepository>();
+            _ = builder.Services.AddScoped<IDoctorRepository, DoctorsRepository>();
+            _ = builder.Services.AddScoped<IPatientService, PatientService>();
+            _ = builder.Services.AddScoped<IDoctorService, DoctorService>();
+        }
+
         private static void ConfigureMiddleware(WebApplication app)
         {
             _ = app.UseMiddleware<ExceptionHandlerMiddleware>();
