6e7947a3d5d2b4aabbb24b5b9e80a654f19de51e
RudMike <nesdavaisa@gmail.com>
2023-06-29 21:46:46 +0300

    Add RoomConfiguration

---
+++ TestTask_Roman/Data/Configurations/RoomConfiguration.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RoomConfiguration.cs" company="RudMike">
+//     Author: Mike Rudnikov
+//     Copyright (c) RudMike. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TestTask_Roman.Data.Models;
+
+namespace TestTask_Roman.Data.Configurations
+{
+    /// <summary>
+    /// Configures the database schema for the <see cref="Room"/> entity.
+    /// </summary>
+    public class RoomConfiguration : IEntityTypeConfiguration<Room>
+    {
+        /// <summary>
+        /// Configures the database schema for the <see cref="Room"/> entity.
+        /// </summary>
+        /// <param name="builder">The builder used to configure the entity.</param>
+        public void Configure(EntityTypeBuilder<Room> builder)
+        {
+            _ = builder.ToTable(DbObjectConstants.Rooms);
+
+            _ = builder.HasKey(room => room.Id);
+
+            _ = builder.Property(room => room.Id)
+                .HasColumnName(DbObjectConstants.RoomId)
+                .ValueGeneratedNever();
+        }
+    }
+}
