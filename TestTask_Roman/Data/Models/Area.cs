8d3f2994b4891014a00bf046b0b10aa7665800f3
RudMike <nesdavaisa@gmail.com>
2023-06-29 21:45:14 +0300

    Add Area model

---
+++ TestTask_Roman/Data/Models/Area.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Area.cs" company="RudMike">
+//     Author: Mike Rudnikov
+//     Copyright (c) RudMike. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TestTask_Roman.Data.Models
+{
+    /// <summary>
+    /// Represents a geographic medical area.
+    /// </summary>
+    public class Area
+    {
+        /// <summary>
+        /// Gets or sets the number of the geographic medical area.
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
