76c44b893d1fdfb497eb30a5e99b1293203a1b15
RudMike <nesdavaisa@gmail.com>
2023-06-30 03:57:24 +0300

    Add IMapper

---
+++ TestTask_Roman/Infrastructure/Mapping/IMapper.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IMapper.cs" company="RudMike">
+//     Author: Mike Rudnikov
+//     Copyright (c) RudMike. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TestTask_Roman.Infrastructure.Mapping
+{
+    /// <summary>
+    /// Interface for mapping between two types.
+    /// </summary>
+    /// <typeparam name="TFrom">The type of the source object.</typeparam>
+    /// <typeparam name="TTo">The type of the destination object.</typeparam>
+    public interface IMapper<TFrom, TTo>
+    {
+        /// <summary>
+        /// Maps the source object to the destination object.
+        /// </summary>
+        /// <param name="from">The source object.</param>
+        /// <returns>The destination object.</returns>
+        public TTo Map(TFrom from);
+    }
+}
