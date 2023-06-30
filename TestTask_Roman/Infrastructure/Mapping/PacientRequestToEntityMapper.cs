3d09fa17d35dcc250273103f30de30baebf74eca
RudMike <nesdavaisa@gmail.com>
2023-06-30 04:00:29 +0300

    Add PacientRequestToEntityMapper

---
+++ TestTask_Roman/Infrastructure/Mapping/PacientRequestToEntityMapper.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PacientRequestToEntityMapper.cs" company="RudMike">
+//     Author: Mike Rudnikov
+//     Copyright (c) RudMike. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using TestTask_Roman.Data.Models;
+using TestTask_Roman.Models;
+
+namespace TestTask_Roman.Infrastructure.Mapping
+{
+    /// <summary>
+    /// Maps between a <see cref="PatientRequest"/> object and a <see cref="Patient"/> entity.
+    /// </summary>
+    public class PacientRequestToEntityMapper : IMapper<PatientRequest, Patient>
+    {
+        /// <inheritdoc/>
+        public Patient Map(PatientRequest from)
+        {
+            var patient = new Patient()
+            {
+                Id = from.Id,
+                FirstName = from.FirstName,
+                LastName = from.LastName,
+                MiddleName = from.MiddleName,
+                Address = from.Address,
+                AreaId = from.Area,
+                BirthDate = from.BirthDate,
+                Sex = from.Sex,
+            };
+
+            return patient;
+        }
+    }
+}
